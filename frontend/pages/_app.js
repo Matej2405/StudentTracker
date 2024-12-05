79eb256c44db68fb4a0f8e3f2038a145e37264a4
Matej2405 <123963920+Matej2405@users.noreply.github.com>
2024-12-05 16:29:59 +0100

    first commit

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <title>Airbnb Clone</title>
+                <title>Airbnb  Clone</title>
             </Head>
                 <Component {...pageProps} />
         </>
